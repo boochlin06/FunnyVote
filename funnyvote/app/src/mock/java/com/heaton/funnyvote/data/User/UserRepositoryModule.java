8bfc0a54c745c0648e12bba6cb4b40da06ee6ede
Heaton Lin <gamejeffjeff@gmail.com>
2018-08-21 23:24:00 +0800

    fix mock test-2

--- funnyvote/app/src/mock/java/com/heaton/funnyvote/data/User/UserRepositoryModule.java
+++ funnyvote/app/src/mock/java/com/heaton/funnyvote/data/User/UserRepositoryModule.java
@@ -5,14 +5,13 @@ import android.content.Context;
 import com.heaton.funnyvote.data.Local;
 import com.heaton.funnyvote.data.Remote;
 import com.heaton.funnyvote.data.user.RemoteUserDataSource;
-import com.heaton.funnyvote.data.user.SPUserDataSource;
+import com.heaton.funnyvote.data.user.LocalUserDataSource;
 import com.heaton.funnyvote.data.user.UserDataSource;
 import com.heaton.funnyvote.database.DaoSession;
 import com.heaton.funnyvote.database.UserDao;
 
 import javax.inject.Singleton;
 
-import dagger.Binds;
 import dagger.Module;
 import dagger.Provides;
 
